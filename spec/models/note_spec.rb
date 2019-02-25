34c44ee6a8d859933e848d875d0dbb75d92fb003
harapeko <github@harapeko.me>
2019-02-26 04:59:03 +0900

    beforeを使ってリファクタリングしたところ

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,33 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-	# 検索文字列に一致するメモを返すこと
-  it "returns notes that match the search term" do
-		user = User.create(
+	before do
+		@user = User.create(
 			first_name: "Joe",
 			last_name: "Tester",
 			email: "joetester@example.com",
 			password: "password",
 		)
 
-		project = user.projects.create(
+		@project = @user.projects.create(
 			name: "Test Project",
 		)
+	end
 
-		note1 = project.notes.create(
-			message: "This is the first note.",
-			user: user,
-		)
-		note2 = project.notes.create(
-			message: "This is the second note.",
-			user: user,
-		)
-		note3 = project.notes.create(
-			message: "First, preheat the oven.",
-			user: user,
-		)
+	# バリデーションのテスト
 
-		expect(Note.search("first")).to include(note1, note3)
-		expect(Note.search("first")).to_not include(note2)
+	# 文字列に一致するメッセージを検索する
+	describe "search message for a term" do
+		before do
+			@note1 = @project.notes.create(
+				message: "This is the first note.",
+				user: @user,
+			)
+			@note2 = @project.notes.create(
+				message: "This is the second note.",
+				user: @user,
+			)
+			@note3 = @project.notes.create(
+				message: "First, preheat the oven.",
+				user: @user,
+			)
+		end
+
+		# 一致するデータが見つかるとき
+		context "when a match is found" do
+			# 検索文字列に一致するメモを返すこと
+			it "returns notes that match the search term" do
+				expect(Note.search("first")).to include(@note1, @note3)
+				expect(Note.search("first")).to_not include(@note2)
+			end
+		end
+
+		# 一致するデータが見つからないとき
+		context "when a match is found" do
+			# 空のコレクションを返すこと
+			it "returns an empty collection" do
+				expect(Note.search("message")).to be_empty
+			end
+		end
 	end
 end
